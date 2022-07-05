c562d335589b9b0a724d5a6c2dedc955cf31cd0b
kosKKH <104198846+kosKKH@users.noreply.github.com>
2022-07-05 15:17:06 +0900

    07-05

--- PUVG/src/main/puvg/approval/controller/ApprovalController.java
+++ PUVG/src/main/puvg/approval/controller/ApprovalController.java
@@ -10,8 +10,38 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import main.puvg.approval.dao.ApprovalDAO;
+import main.puvg.approval.service.ApprovalService;
+import main.puvg.approval.vo.ApprovalListVO;
 
 @RestController
 public class ApprovalController {
+	
+	Logger logger = LogManager.getLogger(ApprovalController.class);
+	
+	@Autowired(required=false)
+	ApprovalService approvalService;
+	
+	ModelAndView mav = new ModelAndView();
+	
+	@GetMapping(value="ApprovalListSelectAll")
+	public ModelAndView ApprovalListSelectAll(ApprovalListVO avo) {
+		
+		logger.info("ApprovalController ApprovalListSelectAll 함수진입");
+		
+		List<ApprovalListVO> listAll = approvalService.ApprovalListSelectAll(avo);
+		
+		if(listAll.size() > 0) {
+			logger.info("ApprovalController ApprovalListSelectAll listAll");
+			
+			mav.addObject("listAll", listAll);
+			mav.addObject("avo", avo);
+			
+			mav.setViewName("approval/ApprovalListSelectAll");
+			
+			return mav;
+		}
+		mav.setViewName("approval/ApprovalListSelectAll");
+		return mav;
+	}
 
 }
